78b7d21cfbb16eef114a1faead42b5ec78cfd65e
Maaz Ansari <maazansari25667@gmail.com>
2025-09-07 03:18:03 +0530

    feat: add db/seed.js script to programmatically seed DB

---
+++ db/seed.js
@@ -0,0 +1,8 @@
+﻿const mysql = require('mysql2/promise');
+const fs = require('fs');
+(async () => {
+  const conn = await mysql.createConnection({host:'localhost',user:'root',password:'password'});
+  const sql = fs.readFileSync('./scripts/seed.sql','utf8');
+  await conn.query(sql);
+  await conn.end();
+})();
